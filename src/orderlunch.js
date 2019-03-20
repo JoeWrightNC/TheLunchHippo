3308c5111b6cf50f58bd8647a41c5ffed988c6db
Joe Wright <joe.wright@samanage.com>
2019-03-20 11:09:19 -0400

    okay

--- src/orderlunch.js
+++ src/orderlunch.js
@@ -13,18 +13,19 @@ const lunchHippoFactory = () => (user) => new Promise((resolve, reject) => {
 
     doc.useServiceAccountAuth(creds, function (err) {
         // Get all of the rows from the spreadsheet.
-        if (user.user_id) {
             doc.getRows(1, { query: `slack="${user.user_id}"` }, function(err, rows) {
-                rows[0].order = user.text;
-                rows[0].save();
+                if (rows) {
+                    rows[0].order = user.text;
+                    rows[0].save();
+                    returnText = `Your lunch order is: "${user.text}".  Sounds like a good lunch to share with your friendly neighborhood hippo.`
+                } else {
+                    returnText = "You have to join the lunch program first by typing /hippostart!"
+                }
+                if(err) {
+                    console.log(err);
+                }
             });
-            if(err) {
-                console.log(err);
-            }
-            returnText = `Your lunch order is: "${user.text}".  Sounds like a good lunch to share with your friendly neighborhood hippo.`;
-        } else {
-            returnText = "You have to join the lunch program first by typing /hippostart!"
-        }
+       
         return resolve({
             text: returnText,
         })
