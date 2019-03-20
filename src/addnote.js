714ccd9cfe759d4b5eb02e6f550693a12547b779
Joe Wright <joe.wright@samanage.com>
2019-03-20 10:14:02 -0400

    removing time validations on server level

--- src/addnote.js
+++ src/addnote.js
@@ -11,20 +11,15 @@ var returnText = ""
 
 const lunchHippoFactory = () => (user) => new Promise((resolve, reject) => {
     doc.useServiceAccountAuth(creds, function (err) {
-        // Get all of the rows from the spreadsheet.
-        if (time < 21){
-            doc.getRows(1, { query: `slack="${user.user_id}"` }, function(err, rows) {
-                rows[0].notes = user.text;
-                rows[0].save();
-                if(err) {
-                    console.log(err);
-                }
-            });
-            returnText = `The notes for your lunch order are: "${user.text}". Pretty Hip(po).`
-        }
-        else {
-            returnText = "Sorry, but it's too late to place your order.  Join us next time though!"
-        }
+    // Get all of the rows from the spreadsheet.
+        doc.getRows(1, { query: `slack="${user.user_id}"` }, function(err, rows) {
+            rows[0].notes = user.text;
+            rows[0].save();
+            if(err) {
+                console.log(err);
+            }
+        });
+        returnText = `The notes for your lunch order are: "${user.text}". Pretty Hip(po).`
         return resolve({
             text: returnText,
         })
